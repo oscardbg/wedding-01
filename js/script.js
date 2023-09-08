64e244b1c7af9ec0e86ee78a6ecdd5684f77135f
Vieniamin <vieniamin@mail.com>
2023-09-08 13:02:03 -0500

    Project structure and base design ready

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const song = document.querySelector("audio");
+window.onload = () => (song.volume = "0.2");
+
+const finalDate = new Date("January 1, 2024 12:00:00");
+
+const getElem = (elem) => {
+  return document.querySelector(elem);
+};
+
+const dayElem = getElem(".days span");
+const hourElem = getElem(".hours span");
+const minElem = getElem(".mins span");
+const segElem = getElem(".segs span");
+
+const calcDate = () => {
+  let now = new Date().getTime();
+  let gap = finalDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  dayElem.textContent = d;
+  hourElem.textContent = h < 10 ? "0" + h : h;
+  minElem.textContent = m < 10 ? "0" + m : m;
+  segElem.textContent = s < 10 ? "0" + s : s;
+};
+
+setInterval(calcDate, 1000);
